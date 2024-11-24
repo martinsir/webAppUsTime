98354e04c524c1ed68e235461707d8ff6ae8a53c
ohjenssen <oskar_jenssen@hotmail.com>
2024-11-24 11:17:56 +0100

    comments

--- scripts/header.js
+++ scripts/header.js
@@ -1,8 +1,10 @@
 export function header(){
 
+    // Selects the hamburger icon and mobile menu
     const menu_btn = document.querySelector('.hamburger');
     const mobile_menu = document.querySelector('.mobile-nav');
 
+    // Adds an event listener that makes the mobile menu appear when clicked
     menu_btn.addEventListener('click', function (){
         menu_btn.classList.toggle('is-active');
         mobile_menu.classList.toggle('is-active');
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
 import { header } from "./header.js";
-
+// Added this index file and importing from other js files so 
+// that we dont have 10 different scripts in the body of the html
 window.onload = header();
