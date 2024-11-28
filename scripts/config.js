08acc8334a6794a4877eaf4c0baced4d55d8c42b
martinsir <martin.hana88@gmail.com>
2024-11-28 14:31:16 +0100

    config.js eksempel

---
+++ scripts/config.js
@@ -0,0 +1,10 @@
+const config = {
+    DB_HOST: 'localhost', // Din database-host
+    DB_USER: 'din_bruger', // Din databasebrugers navn
+    DB_PASSWORD: 'din_adgangskode', // Din adgangskode
+    DB_NAME: 'SafeDialogDB', // Dit databasenavn - hele navnet
+    PORT: 3000 // Din applikationsport
+  };
+  
+  module.exports = config;
+  
